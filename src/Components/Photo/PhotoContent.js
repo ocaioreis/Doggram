a743403a01ba541fbdb84a7b0c371f1d7ee51ea2
Caio Reis <caioace@gmail.com>
2023-03-17 18:01:14 -0300

    Modal adjust
    Modal abrindo e fechando corretamente

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -13,7 +13,7 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
@@ -23,7 +23,7 @@ const PhotoContent = ({ data }) => {
           <ul className={styles.attributes}>
             <li>{photo.peso} kg</li>
             <li>
-              {photo.idade} {photo.idade > 1 ? "ano" : "anos"}
+              {photo.idade} {photo.idade < 1 ? "ano" : "anos"}
             </li>
           </ul>
         </div>
